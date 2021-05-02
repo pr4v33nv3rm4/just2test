fe7004a5f7445ab5d624977bbf6eb6f21f1fc1e8
Praveen Verma <Praveen.Verma@nectechnologies.in>
2021-05-02 13:09:47 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        private double varA { get; set; }
+        private double varB { get; set; }
+
+        public Calculator(double a,double b)
+        {
+            varA = a;
+            varB = b;
+        }
+
+        //method to add
+        public double Add(){
+            return (varA+varB);
+        }
+        //subtart 
+        public double Subtract()
+        {
+            return (varA-varB);
+        }
+        //Multiply
+        public double Multiply()
+        {
+            return (varA*varB);
+        }
+        //Devide
+        public double Devide()
+        {
+            return (varA/varB);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Calculator calc = new Calculator(double.Parse(args[0]), double.Parse(args[1]));
+                Console.WriteLine($"Add :{calc.Add()}");
+                Console.WriteLine($"Subtract :{calc.Subtract()}");
+                Console.WriteLine($"Multiply :{calc.Multiply()}");
+                Console.WriteLine($"Devide :{calc.Devide()}");
+
+            }
+            else {
+                Console.WriteLine("no argument passed!");
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
